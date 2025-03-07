899e8b785dbdad62aed2e8799149d432de35965f
Lam Thien Phat <164598319+NeikoYekindar@users.noreply.github.com>
2025-03-07 23:34:51 +0700

    Add files via upload

---
+++ attackable.h
@@ -0,0 +1,16 @@
+#pragma one
+#include<iostream>
+#include<string>
+using namespace std;
+class attackable{
+    protected:  
+        string ten;
+        int level;
+    public:
+        attackable();
+        ~attackable();
+        virtual void nhap();
+        virtual void xuat()const;
+        virtual long long tinhsatthuong();
+        virtual string xuathe();
+};
---
+++ game.h
@@ -0,0 +1,16 @@
+#include"attackable.h"
+#include<iostream>
+#include<string>
+using namespace std;
+class game{
+    protected:
+        attackable**dsnv;
+        int sl;
+    public:
+        game();
+        ~game();
+        void nhap();
+        void xuat()const;
+        void satthuongcaonhat();
+        void thuoctinhtancong();
+};
